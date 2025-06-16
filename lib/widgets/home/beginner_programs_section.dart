9ba142dcaed18e371651850af76a5db0d71bc1d4
Golden76z <damienprouet76@gmail.com>
2025-06-16 11:03:01 +0200

    Refactoring tree

--- lib/widgets/home/beginner_programs_section.dart
+++ lib/widgets/home/beginner_programs_section.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sport_app/app_theme.dart';
+import 'package:sport_app/core/theme/app_theme.dart';
 
 class BeginnerProgramsSection extends StatelessWidget {
   const BeginnerProgramsSection({super.key});
